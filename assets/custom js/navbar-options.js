281c496edfd1ea285473dab950cf8e6f7aa5fe25
Kaiserkrow <42108060+Kaiserkrow@users.noreply.github.com>
2022-08-08 04:24:11 +0800

    finally added the navbar feature

--- assets/custom js/navbar-options.js
+++ assets/custom js/navbar-options.js
@@ -1,32 +1,46 @@
 document.lastScrollPosition = 0;
 document.lastCentered = 0;
 document.onWayTo = null;
+let sections = document.getElementById("sections");
 
-document.addEventListener("scroll", () => {
-  const direction =
-    window.pageYOffset - document.lastScrollPosition > 0 ? "down" : "up";
-  const sections = [...document.querySelectorAll("section")];
+sections.addEventListener("scroll", () => {
+  let everyUnderline = [...document.getElementsByClassName("underline")];
+  const height = sections.offsetHeight;
+  let scroll = sections.scrollTop;
+  const direction = scroll - document.lastScrollPosition > 0 ? "down" : "up";
 
-  if (document.onWayTo === null) {
-    const destIndex =
-      direction === "up"
-        ? document.lastCentered - 1
-        : document.lastCentered + 1;
-    if (destIndex >= 0 && destIndex < sections.length) {
-      console.log(`${destIndex}  ${direction}`);
-      document.onWayTo = destIndex;
-      window.scroll(0, sections[destIndex].offsetTop);
-    }
+  console.log(direction);
+
+  console.log(`height is: ${height} and scroll is ${scroll}`);
+  if (scroll === 0 || scroll < height*.5) {
+    everyUnderline[0].classList.add("active");
+    everyUnderline[1].classList.remove("active");
+    everyUnderline[2].classList.remove("active");
+    everyUnderline[3].classList.remove("active");
+    everyUnderline[4].classList.remove("active");
+  } else if (scroll > height - height / 2 && scroll < height*1.5) {
+    everyUnderline[0].classList.remove("active");
+    everyUnderline[1].classList.add("active");
+    everyUnderline[2].classList.remove("active");
+    everyUnderline[3].classList.remove("active");
+    everyUnderline[4].classList.remove("active");
+  } else if (scroll > (height * 1) + height / 2 && scroll < height * 2.5) {
+    everyUnderline[0].classList.remove("active");
+    everyUnderline[1].classList.remove("active");
+    everyUnderline[2].classList.add("active");
+    everyUnderline[3].classList.remove("active");
+    everyUnderline[4].classList.remove("active");
+  } else if (scroll > (height * 2) + height / 2 && scroll < height * 3.5) {
+    everyUnderline[0].classList.remove("active");
+    everyUnderline[1].classList.remove("active");
+    everyUnderline[2].classList.remove("active");
+    everyUnderline[3].classList.add("active");
+    everyUnderline[4].classList.remove("active");
+  } else if (scroll > height * 3 && scroll < height * 4) {
+    everyUnderline[0].classList.remove("active");
+    everyUnderline[1].classList.remove("active");
+    everyUnderline[2].classList.remove("active");
+    everyUnderline[3].classList.remove("active");
+    everyUnderline[4].classList.add("active");
   }
-
-  sections.forEach((section, index) => {
-    if (window.pageYOffset === section.offsetTop) {
-      document.lastCentered = index;
-      if (document.onWayTo === index) {
-        document.onWayTo = null;
-      }
-    }
-  });
-
-  document.lastScrollPosition = window.pageYOffset;
 });
