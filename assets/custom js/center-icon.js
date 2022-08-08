b84e4b1d37f45a405e56780289e189a59a08b808
Kaiserkrow <42108060+Kaiserkrow@users.noreply.github.com>
2022-08-08 20:39:53 +0800

    main page version 1 is done

--- assets/custom js/center-icon.js
+++ assets/custom js/center-icon.js
@@ -10,63 +10,41 @@ sections.addEventListener("scroll", () => {
   let scroll = sections.scrollTop;
   const direction = scroll - document.lastScrollPosition > 0 ? "down" : "up";
 
-  console.log(direction);
-  console.log(allSection)
-  console.log(`height is: ${height} and scroll is ${scroll}`);
+
   if (scroll === 0 || scroll < height*.5) {
     allSection[0].classList.add("animate-active");
     allSection[1].classList.remove("animate-active");
     allSection[2].classList.remove("animate-active");
     allSection[3].classList.remove("animate-active");
     allSection[4].classList.remove("animate-active");
-    everyUnderline[0].classList.add("active");
-    everyUnderline[1].classList.remove("active");
-    everyUnderline[2].classList.remove("active");
-    everyUnderline[3].classList.remove("active");
-    everyUnderline[4].classList.remove("active");
+
   } else if (scroll > height - height / 2 && scroll < height*1.5) {
     allSection[0].classList.remove("animate-active");
     allSection[1].classList.add("animate-active");
     allSection[2].classList.remove("animate-active");
     allSection[3].classList.remove("animate-active");
     allSection[4].classList.remove("animate-active");
-    everyUnderline[0].classList.remove("active");
-    everyUnderline[1].classList.add("active");
-    everyUnderline[2].classList.remove("active");
-    everyUnderline[3].classList.remove("active");
-    everyUnderline[4].classList.remove("active");
+
   } else if (scroll > (height * 1) + height / 2 && scroll < height * 2.5) {
     allSection[0].classList.remove("animate-active");
     allSection[1].classList.remove("animate-active");
     allSection[2].classList.add("animate-active");
     allSection[3].classList.remove("animate-active");
     allSection[4].classList.remove("animate-active");
-    everyUnderline[0].classList.remove("active");
-    everyUnderline[1].classList.remove("active");
-    everyUnderline[2].classList.add("active");
-    everyUnderline[3].classList.remove("active");
-    everyUnderline[4].classList.remove("active");
+
   } else if (scroll > (height * 2) + height / 2 && scroll < height * 3.5) {
     allSection[0].classList.remove("animate-active");
     allSection[1].classList.remove("animate-active");
     allSection[2].classList.remove("animate-active");
     allSection[3].classList.add("animate-active");
     allSection[4].classList.remove("animate-active");
-    everyUnderline[0].classList.remove("active");
-    everyUnderline[1].classList.remove("active");
-    everyUnderline[2].classList.remove("active");
-    everyUnderline[3].classList.add("active");
-    everyUnderline[4].classList.remove("active");
+
   } else if (scroll > height * 3 && scroll < height * 4) {
     allSection[0].classList.remove("animate-active");
     allSection[1].classList.remove("animate-active");
     allSection[2].classList.remove("animate-active");
     allSection[3].classList.remove("animate-active");
     allSection[4].classList.add("animate-active");
-    everyUnderline[0].classList.remove("active");
-    everyUnderline[1].classList.remove("active");
-    everyUnderline[2].classList.remove("active");
-    everyUnderline[3].classList.remove("active");
-    everyUnderline[4].classList.add("active");
+
   }
 });
